49d09c5c750dd5977e82b92e204a06286ebd16cf
BilousVladyslav <mytwink.steam@gmail.com>
2019-12-28 00:48:00 +0200

    created main component`s templates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './modules/profile/profile.component';
+import { VideoStreamComponent } from './modules/video-stream/video-stream.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { ContentComponent } from './shared/components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    VideoStreamComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
