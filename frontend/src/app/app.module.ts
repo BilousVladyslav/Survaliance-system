3b0caf3d02b99c70eec4fd8ed28a6917c349a594
BilousVladyslav <mytwink.steam@gmail.com>
2019-12-28 02:07:11 +0200

    added bootstrap, material, toolbar template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,20 +7,25 @@ import { VideoStreamComponent } from './modules/video-stream/video-stream.compon
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { ContentComponent } from './shared/components/content/content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    VideoStreamComponent,
-    FooterComponent,
-    NavbarComponent,
-    ContentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProfileComponent,
+        VideoStreamComponent,
+        FooterComponent,
+        NavbarComponent,
+        ContentComponent,
+        
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
